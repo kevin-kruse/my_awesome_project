bc8113fe44e9437a8ab9257b6b9633c136052fc2
kevin-kruse <kevin-kruse@users.noreply.github.com>
2023-11-22 18:14:13 +0000

    ✅ Ready to clone and code.

---
+++ my_awesome_project/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for my_awesome_project."""
+
+from my_awesome_project.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
-from project_name.base import NAME
+from my_awesome_project.base import NAME
 
 
 def test_base():
-    assert NAME == "project_name"
+    assert NAME == "my_awesome_project"
